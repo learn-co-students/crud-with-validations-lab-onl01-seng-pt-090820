12fca00f740768c30d16f7751f39cdbf67d71b4c
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-01-12 16:56:39 -0700

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,17 @@
+class Song < ApplicationRecord
+    validates :title, :artist_name, presence: true
+    validates :title, uniqueness: { scope: :release_year }
+    validates :release_year, presence: true, if: :released?
+    validate :release_year_cannot_be_in_future
+
+    def released?
+        released == true
+    end
+
+    def release_year_cannot_be_in_future
+        if release_year.present? && release_year > Date.today.year
+            errors.add(:release_year, "can't be in the future")
+        end
+    end
+
+end
