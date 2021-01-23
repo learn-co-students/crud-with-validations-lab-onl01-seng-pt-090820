a0fdc0fb5e5f0253369891787924f6f25f3bd4be
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-01-22 17:12:41 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,17 +1,19 @@
 class Song < ApplicationRecord
     validates :title, :artist_name, presence: true
-    validates :title, uniqueness: { scope: :release_year }
-    validates :release_year, presence: true, if: :released?
-    validate :release_year_cannot_be_in_future
+    validates :title, uniqueness: {scope: [:artist_name, :release_year]}
+    validates :released, inclusion: [true, false]
+    validates :release_year, presence: true, if: :released
+    validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released
+    # validate :release_year_cannot_be_in_future
 
-    def released?
-        released == true
-    end
+    # def released?
+    #     released == true
+    # end
 
-    def release_year_cannot_be_in_future
-        if release_year.present? && release_year > Date.today.year
-            errors.add(:release_year, "can't be in the future")
-        end
-    end
+    # def release_year_cannot_be_in_future
+    #     if release_year.present? && release_year > Date.today.year
+    #         errors.add(:release_year, "can't be in the future")
+    #     end
+    # end
 
 end
