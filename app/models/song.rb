1842b5295dced17ee35fbc9d80639713051e3ce6
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-02-01 20:14:01 -0600

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ApplicationRecord
+
+    validates :title, :artist_name, presence: true
+    validates :released, inclusion: [true, false]
+    validates :title, uniqueness: { scope: [:release_year, :artist_name],
+    message: "should happen once per year" }
+    validates :release_year, presence: true, if: :released
+    validates :release_year, numericality: {less_than_or_equal_to: 2021}, if: :released
+end
